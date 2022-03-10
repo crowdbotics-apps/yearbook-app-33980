e01bbaaae120bd9cd224465b046ab725033bf6b2
Swornim Shrestha <swornim@Swornims-MacBook-Pro.local>
2022-03-10 21:29:33 +0545

    Email Template Fixed

--- yearbook/urls.py
+++ yearbook/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
-from .views import RecappViewSet, UploadHighSchoolIdViewSet,HighSchoolsViewset
+from .views import PurchaseRecappViewSet, RecappViewSet, UploadHighSchoolIdViewSet,HighSchoolsViewset
 
 router = DefaultRouter()
 router.register(r'highschools',HighSchoolsViewset,basename="high_schools")
 router.register(r'upload_id', UploadHighSchoolIdViewSet,basename="upload_id")
 router.register(r'recapp',RecappViewSet,basename="recapp")
+router.register(r'purchase',PurchaseRecappViewSet,basename="recapp")
 urlpatterns = [
     path("",include(router.urls)),
     path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
